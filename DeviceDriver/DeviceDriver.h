f0730cea2c0725214a697e76e0260553e60d9f44
hsoo.choi <choihszg@gmail.com>
2023-07-07 13:30:37 +0900

    device driver read

--- DeviceDriver/DeviceDriver.h
+++ DeviceDriver/DeviceDriver.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <exception>
 #include "FlashMemoryDevice.h"
+#include <iostream>
+using namespace std;
 
 class DeviceDriver
 {
@@ -10,4 +13,14 @@ public:
 
 protected:
     FlashMemoryDevice* m_hardware;
+};
+
+class ReadFailException : public std::exception
+{
+public:
+	explicit ReadFailException(char const* _Message)
+		: exception(_Message)
+	{
+		cout << _Message << endl;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,56 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+using namespace testing;
+using namespace std;
 
+#include "../DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class MockDevice : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long adress, unsigned char data), (override));
+};
+
+class TestFixture : public testing::Test
+{
+public:
+	MockDevice md;
+	DeviceDriver *dd;
+
+	void assertIllegalRead(long address)
+	{
+		try
+		{
+			dd->read(address);
+			FAIL();
+		}
+		catch(ReadFailException e)
+		{
+		}
+	}
+protected:
+	void SetUp() override
+	{
+		dd = new DeviceDriver(&md);
+	}
+};
+
+TEST_F(TestFixture, ReadTestPass) {
+	EXPECT_CALL(md, read)
+		.Times(5)
+		.WillRepeatedly(Return(10));		
+
+	EXPECT_EQ(10, dd->read(0xA));
+}
+
+TEST_F(TestFixture, ReadTestFailWithException) {
+	EXPECT_CALL(md, read)
+		.WillOnce(Return(10))
+		.WillOnce(Return(10))
+		.WillOnce(Return(10))
+		.WillOnce(Return(10))
+		.WillOnce(Return(20));
+
+	assertIllegalRead(0xA);
 }
